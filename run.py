2af0cbf53f8372f59e316a6c3f63a9f4084f5030
megahomyak <g.megahomyak@gmail.com>
2024-12-09 04:37:15 +0500

    Socketfuck

--- run.py
+++ run.py
@@ -1,14 +1,30 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit
+from PyQt6.QtCore import Qt
+import socket
 
-class HFCFWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+invoker = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+invoker.bind("invoke")
+try:
+    class HFCFWindow(QMainWindow):
+        def __init__(self):
+            super().__init__()
+            self.inputfield = QLineEdit(self)
+            font = self.inputfield.font()
+            font.setPointSize(30)
+            self.inputfield.setFont(font)
+        def resizeEvent(self, event):
+            self.inputfield.resize(self.width() - 30, 60)
+            self.inputfield.move(15, 15)
+            super().resizeEvent(event)
+        def keyPressEvent(self, event):
+            if event.key() == Qt.Key.Key_Escape:
+                self.hide()
+            else:
+                super().keyPressEvent(event)
 
-        inputfield = QLineEdit()
-
-        self.setCentralWidget(inputfield)
-
-app = QApplication([])
-window = HFCFWindow()
-window.showFullScreen()
-app.exec()
+    app = QApplication([])
+    window = HFCFWindow()
+    window.show()
+    app.exec()
+finally:
+    invoker.close()
