451542e7f5ca3dadf23469ff52e89ddc26d6cb10
megahomyak <g.megahomyak@gmail.com>
2024-12-09 18:38:15 +0500

    Made "invoke" accept the pipe path for convenience

--- invoke.c
+++ invoke.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
-void main(void) {
-    fclose(fopen("invoke", "w"));
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        FILE* file = fopen(argv[1], "w");
+        if (file == NULL) return 1;
+        else {
+            fclose(file);
+            return 0;
+        }
+    } else {
+        return 1;
+    }
 }
