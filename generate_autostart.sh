fc10cff49137f9c6ef8e3c99ab7c4b87a02f5f88
megahomyak <g.megahomyak@gmail.com>
2024-12-09 19:22:07 +0500

    Fixed the autostart script a bunch

--- generate_autostart.sh
+++ generate_autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-DIR=$1
-if [ "$DIR" == "" ]; then
+DIR="$1"
+if [ "$DIR" = "" ]; then
 >&2 echo "Directory wasn't passed"
 else
-cat > ~/.config/hfcf.desktop << EOF
+cat > ~/.config/autostart/hfcf.desktop << EOF
 [Desktop Entry]
-Exec=bash -c 'cd ${DIR} && poetry run python run.py'
+Exec=sh -c 'cd ${DIR} && poetry run python run.py'
 Terminal=false
 Type=Application
 Name=hfcf
